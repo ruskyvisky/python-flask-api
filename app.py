eb11b4c4c84375e5c0e74065d70a4e70714b990a
ruskyvisky <numan_859@hotmail.com>
2025-10-31 21:29:52 +0300

    refactor: clean up comments and remove unnecessary lines in app.py and test_api.py

--- app.py
+++ app.py
@@ -1,12 +1,7 @@
-# app.py
 from flask import Flask, jsonify
-
 app = Flask(__name__)
-
 @app.route('/', methods=['GET'])
 def home():
     return jsonify({"message": "Merhaba, Dünya!"})
-
 if __name__ == '__main__':
-
     app.run(host='0.0.0.0', port=5000)
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,19 +1,12 @@
 import pytest
-# app.py dosyasındaki Flask uygulamasını içe aktarıyoruz
 from app import app 
 @pytest.fixture
 def client():
-    # Flask uygulamasını test moduna al
     app.config['TESTING'] = True
-    # Bir test istemcisi oluştur
     with app.test_client() as client:
-        yield client # Testlere istemciyi sağla
-
+        yield client 
 def test_home_route(client):
-    # Kök adrese GET isteği gönder
     response = client.get('/')
-    
-    # Yanıt durum kodunu kontrol et
     assert response.status_code == 200
     data = response.get_json()
     assert data['message'] == "Merhaba, Dünya!"
